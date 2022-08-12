5ac4d633060ec441fe8da3536780c2414b4f65e9
FananTamim <110988214+FananTamim@users.noreply.github.com>
2022-08-12 18:24:18 +0300

    Update Stack.swift

--- Exercise1/Stack.swift
+++ Exercise1/Stack.swift
@@ -1,2 +1,45 @@
-print("hi")
+import Foundation
+
+class Stack<T> {
+    var items : [T]
+    
+    init(items: [T]) {
+        self.items = items
+    }
+    func push(_ item: T) {
+        items.append(item)
+    }
+    func pop() -> T? {
+        return items.isEmpty ? nil : items.removeLast()
+    }
+    func peek() -> T? {
+        return items.isEmpty ? nil : items[items.count - 1]
+    }
+    func isEmpty() -> Bool {
+        return items.isEmpty
+    }
+}
+
+class Node {
+    var x : Int
+    init(x: Int) {
+        self.x = x
+    }
+}
+var n1 = Node(x: 4)
+var n2 = Node(x: 3)
+var myStack = Stack<Node>()
+myStack.push(n1)
+print(myStack.items)
+myStack.push(n2)
+myStack.pop()
+print(myStack.items)
+myStack.isEmpty()
+myStack.pop()
+print(myStack.peek())
+myStack.pop()
+myStack.pop()
+myStack.pop()
+myStack.isEmpty()
+print(myStack.peek())
 
